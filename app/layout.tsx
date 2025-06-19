d2f599ee0c7a96be307b6644d40bce4517d418ea
Loughann <loughanpk2018@gmail.com>
2025-06-19 00:47:28 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Whats Espião',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
